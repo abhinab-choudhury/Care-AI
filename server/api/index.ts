2441e12bb721bf9ab15576b0e9f320a7ef47a7e2
abhinab-choudhury <abhinabchoudhury291@gmail.com>
2024-10-20 20:54:16 +0530

    updated

--- server/api/index.ts
+++ server/api/index.ts
@@ -62,10 +62,7 @@ db_connect().then(() => {
     res.status(200).json(new ApiResponse(200, 'Server is Healthy'));
     return;
   });
-  app.get('/', (req: Request, res: Response) => {
-    res.send('Hi!!');
-    return;
-  });
+
 });
 
 // Global Catch
